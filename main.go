ca41bceef9c199d659b1af5a4ee84b902919b2b3
Elin <ejyaworski@gmail.com>
2021-04-26 19:38:16 -0400

    Second commit to feature branch. This version is working and satifies the question requirements. It works using a nested for loop to append the string version of an integer to the variable result. Lastly, I print a sliced version of result removing the last element, that is, a commma.

--- main.go
+++ main.go
@@ -7,12 +7,23 @@ import "os"
 
 func main() {
   x := []string{"Zero","One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine"} // list used for indexing; ex: x[1] == One
-  num_args := len(os.Args[1:]) // length of arguments inputted
+  result := ""
 
-  a := os.Args[1] // Getting command line argument
-  fmt.Println(num_args, x[a[0] - 48]) // Test if string, or rune, index is ascii
-
+  for _, i := range os.Args[1:]{ // read each argument in command line
+    for j := 0; j < len(i); j++{ // for each element in array, convert to numerical word
+      result = result + (x[i[j] - 48]) // convert from ascii to integer, access numerical word from list x, append to string 
+    }
+    result = result + "," // appebnd comma to string
+  }
+  result = result[:len(result)-1] // slice away final comma in string
+  fmt.Println(result) // print result
 }
 
 // references:
 // https://gobyexample.com/
+
+// test code: ***Works!***
+// go run main.go 10 300 5
+// OneZero,ThreeZeroZero,Five
+// go run main.go 3 25 209
+// Three,TwoFive,TwoZeroNine
